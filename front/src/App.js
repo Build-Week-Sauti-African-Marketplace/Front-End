49d37059b29d10b4b00da8e630f5b85d15fc102b
thesoundfromthesky <nagara@naver.com>
2019-11-18 16:57:00 -0600

    signup

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Link, Route } from "react-router-dom";
+
+import Login from "./Login/Login";
+import SignUp from "./SignUp/Signup";
 
 function App() {
- 
-return (
-  <div>
-    <Router>
-      <Link to="/Signup"> Sign Up</Link>
-      <Link to="/Login"> Log In</Link>
+  return (
+    <div>
+      <Link to="/signup"> Sign Up</Link>
+      <Link to="/login"> Log In</Link>
       <Link to="/profile"> Profile </Link>
       <Link to="/addProducts"> Add Products</Link>
 
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/SignUp" component={SignUp} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/SignUp" component={SignUp} />
+      {/* <Route exact path="/login" component={Login} />
+         />
         <ProtectedRoute exact path="/profile" component={Profile} />
-        <ProtectedRoute exact path="/addProducts" component={addProducts} />
-    </Router>
-  </div>
-)
+        <ProtectedRoute exact path="/addProducts" component={Products} /> */}
+    </div>
+  );
 }
 
 export default App;
