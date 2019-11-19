577dcc26973cee5fdf7e817829313debd3e2e933
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2019-11-19 17:20:23 -0500

    ready

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Profile from "./profile";
+import Profile from "./Profile";
 import Login from "./Login/Login";
 import SignUp from "./SignUp/Signup";
 import Products from "./Products";
@@ -15,15 +15,16 @@ import ProtectedRoute from "./ProtectedRoute";
 function App() {
   return (
     <div>
+      <Link to="/home">Home</Link>
       <Link to="/signup"> Sign Up</Link>
       <Link to="/login"> Log In</Link>
-      <Link to="/profile"> Profile </Link>
-      <Link to="/addProducts"> Add Products</Link>
+     
 
       <Route exact path="/login" component={Login} />
       <Route exact path="/SignUp" component={SignUp} />
       <Route exact path="/login" component={Login} />
 
+      <ProtectedRoute exact path="/profile/:username" component={Profile} />
       <ProtectedRoute exact path="/profile" component={Profile} />
 
       <ProtectedRoute exact path="/addProducts" component={AddProducts} />
