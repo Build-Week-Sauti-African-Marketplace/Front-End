64f778183111a74b0619a81fbb11aa111cce69d4
msearles25 <skaman4459@gmail.com>
2019-11-19 14:05:29 -0500

    fixed site build error

--- front/src/Login/Login.js
+++ front/src/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Login.css';
-import Profile from "../profile"
+// import Profile from "../profile"
 function Login(props) {
    const [loginForm, setLoginForm] = useState({
        userName: '',
