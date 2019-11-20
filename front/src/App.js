0cf3d25100b3237f0357c85a2181d988afcbb00a
thesoundfromthesky <nagara@naver.com>
2019-11-20 14:59:24 -0600

    before push

--- front/src/App.js
+++ front/src/App.js
@@ -1,28 +1,46 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-// import Profile from "./profile";
-=======
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Link
+} from "react-router-dom";
 import Profile from "./Profile";
->>>>>>> master
 import Login from "./Login/Login";
 import SignUp from "./SignUp/Signup";
 import Products from "./Products/Products";
 import AddProducts from "./addProducts";
 import ProtectedRoute from "./ProtectedRoute";
-import Search from "./Search"
-import Item from "./item"
-import { connect } from "react-redux"
-import axios from "axios"
+import Search from "./Search";
+import Item from "./item";
+import { connect } from "react-redux";
+import axios from "axios";
 
-function App() {
+function App(props) {
+  console.log(props.data);
+  const logOut = e => {
+    e.preventDefault();
+    axios
+      .get("https://africanmarketplace.herokuapp.com/logout", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+      .then(res => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("name");
+
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
+  };
   return (
     <div>
       <nav className="navbar">
         <div className="nav-list">
+          <Link to="/">Home</Link>
           <NavLink to="/signup" activeClassName="active">
             Sign Up
           </NavLink>
@@ -38,50 +56,17 @@ function App() {
           <NavLink to="/addProducts" activeClassName="active">
             Add Products
           </NavLink>
+          <Link to="/search"> Delete a product </Link>
         </div>
       </nav>
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/SignUp" component={SignUp} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/products" component={Products} />
-      {/* <ProtectedRoute exact path="/profile" component={Profile} /> */}
-
-function App(props) {
-  console.log(props.data)
-  const logOut = e => {
-    e.preventDefault()
-    axios.get("https://africanmarketplace.herokuapp.com/logout",
-    {
-        headers: {
-            Authorization:`Bearer ${localStorage.getItem("token")}`}
-    })
-    .then(res => {
-        localStorage.removeItem("token")
-        localStorage.removeItem("name")
-        
-        console.log(res.data)})
-    .catch(err => console.log(err))
-    
-}
-  return (
-    <div>
-
-      <Link to="/">Home</Link>
-      <Link to="/signup"> Sign Up</Link>
-      <Link to="/login"> Log In</Link>
-     <Link to={`/profile/:username`}>Profile</Link>
-     <Link to="/addProducts"> Add a product</Link>
-     <Link to="/search"> Delete a product </Link>
-     <button onClick={logOut}><Link to="/">Log Out</Link></button>
-     <Route exact path="/item" component={Item} />
-
-
+      <button onClick={logOut}>
+        <Link to="/">Log Out</Link>
+      </button>
+      <Route exact path="/item" component={Item} />
+      <Route path="/products" component={Products}></Route>
       <Route exact path="/login" component={Login} />
       <Route exact path="/SignUp" component={SignUp} />
       <ProtectedRoute exact path="/search" component={Search} />
-     
-
-      
 
       <ProtectedRoute exact path={`/profile/:username`} component={Profile} />
       <ProtectedRoute exact path="/profile" component={Profile} />
@@ -90,12 +75,10 @@ function App(props) {
     </div>
   );
 }
-function mapStateToProps (state){
+function mapStateToProps(state) {
   return {
-  data:state.data
-  }
+    data: state.data
+  };
 }
 
-
-export default connect(mapStateToProps) (App);
-
+export default connect(mapStateToProps)(App);
