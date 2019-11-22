249b63df0a53f24f91737dadccd7fd80c458c42b
thesoundfromthesky <nagara@naver.com>
2019-11-21 19:30:10 -0600

    style changes on search.js and addporudctsjs

--- front/src/Search.js
+++ front/src/Search.js
@@ -1,52 +1,61 @@
-import React,{useState,useEffect} from "react"
-import Axios from "axios"
-import { connect } from "react-redux"
-import { fetchData,searchData,deleteItem } from "./actionCreators"
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { connect } from "react-redux";
+import { fetchData, searchData, deleteItem } from "./actionCreators";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Item from "./item"
+import Item from "./item";
+import "./Search.css";
 
 const Search = props => {
-const [input,setInput] = useState("")
+  const [input, setInput] = useState("");
 
+  useEffect(() => {
+    props.fetchData();
+  }, []);
 
-useEffect(() => {
-props.fetchData()
-},[])
+  const searchItems = e => {
+    e.preventDefault();
+    props.searchData(input);
+  };
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
 
-const searchItems = e => {
-    e.preventDefault()
-    props.searchData(input)
-    
+  return (
+    <div className="item-container">
+      <form onSubmit={searchItems}>
+        <ul>
+          {props.data.map(i => (
+            <p className="delete-list" key={i.itemid}>
+              {i.name}
+              <button
+                onClick={e => {
+                  e.preventDefault();
+                  props.deleteItem(i.itemid);
+                }}
+              >
+                delete
+              </button>
+            </p>
+          ))}
+        </ul>
+        <label htmlFor="delete">Delete Item:</label>
+        <input id="delete" name={input} value={input} onChange={handleChange} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    data: state.data
+  };
 }
-const handleChange = e => {
-   
-    setInput(
-        e.target.value
-    )
-}
+const mapDispatchToProps = {
+  fetchData,
+  searchData,
+  deleteItem
+};
 
-    return (
-        <form onSubmit={searchItems}>
-            <ul>
-            {props.data.map(i => <p key={i.itemid}>{i.name}<button onClick={(e) => {
-                e.preventDefault()
-                props.deleteItem(i.itemid)
-                }}>delete</button></p>)}
-            </ul>
-            <input name={input} value={input} onChange={handleChange}/>
-            <button>Submit</button>
-        </form>
-    )
-}
-
-
-function mapStateToProps (state){
-    return {
-    data:state.data
-    }
-  }
-  const mapDispatchToProps = {
-  fetchData,searchData,deleteItem
-  }
-  
-  export default connect(mapStateToProps,mapDispatchToProps)(Search);
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
